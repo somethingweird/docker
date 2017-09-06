94cf123bfe7ff2c4b46c88c043e50c78c0a717c5
Henry Wong <hwong@dja.com>
2017-09-06 14:00:54 -0400

    docker ps -a Updated with json format

--- gui/routes/images/index.js
+++ gui/routes/images/index.js
@@ -3,25 +3,8 @@ var router = express.Router();
 
 var shell = require('child_process');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    // docker images --format "{{.Repository}}\t{{.Tag}}\t{{.ID}}\t{{.CreatedAt}}\t{{.Size}}"
-
-    shell.execFile('docker', ['images', '--format', '{{.Repository}}\t{{.Tag}}\t{{.ID}}\t{{.CreatedAt}}\t{{.Size}}'], function (error, stdout, stderr) {
-        // REPOSITORY-TAG-IMAGE ID-CREATED-SIZE
-        var rows = stdout.split("\n");
-        var a = rows.length - 1;
-        var d = [];
-        for (var i = 0; i < a; i++) {
-            // parse each one into it own cell
-                d.push(rows[i].split("\t"));
-        }
-        res.json({images: d});
-    });
-});
-
 // better way is now
-router.get('/beta', function(req, res, next) {
+router.get('/', function(req, res, next) {
     // docker images --format '{{json .}}' --no-trunc
     shell.execFile('docker', ['images', '--format', '{{json .}}', '--no-trunc'], function (error, stdout, stderr) {
         var rows = stdout.split("\n");
--- gui/routes/ps/index.js
+++ gui/routes/ps/index.js
@@ -3,18 +3,14 @@ var router = express.Router();
 
 var shell = require('child_process');
 
-/* GET home page. */
 router.get('/all', function(req, res, next) {
-    // docker ps -a --format "{{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.RunningFor}}\t{{.Ports}}\t{{.Status}}\t{{.Size}}\t{{.Names}}\t{{.Labels}}\t{{.Mounts}}\t{{.Networks}}" --no-trunc
-    shell.execFile('docker', ['ps', '-a', '--format', '{{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.RunningFor}}\t{{.Ports}}\t{{.Status}}\t{{.Size}}\t{{.Names}}\t{{.Labels}}\t{{.Mounts}}\t{{.Networks}}', '--no-trunc'], function (error, stdout, stderr) {
-
-        var g = stdout.split("\n");
-        var a = g.length - 1;
-        var d = [];
-        for (var i = 0; i < a; i++) {
-            d.push(g[i].split("\t"));
-        }
-        res.json({images: d});
+    shell.execFile('docker', ['ps', '-a', '--format', '{{json .}}', '--no-trunc'], function (error, stdout, stderr) {
+        var rows = stdout.split("\n");
+        rows.pop();
+        var j = rows.map(function (x) {
+            return JSON.parse(x);
+        });
+        res.json({images: j});
     });
 });
 
