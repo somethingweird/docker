c6c1e545b9647927a439e20342ca7ecb7dcd10d1
Henry Wong <hwong@dja.com>
2017-09-06 10:27:42 -0400

    Rest of open-iconic

---
+++ gui/routes/stop/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+var shell = require('child_process');
+
+/* GET home page. */
+router.get('/:tagId', function(req, res, next) {
+    var regex = /^\w+$/;
+    if (regex.test(req.params.tagId)) {
+        shell.execFile('docker', ['stop', req.params.tagId], function (error, stdout, stderr) {
+            if (stderr) {
+                res.json({success: false, message: error});
+            } else {
+                var matchAgainst = req.params.tagId;
+                if (stdout.indexOf(matchAgainst) > -1) {
+                    res.json({success: true, message: stdout});
+                } else {
+                    res.json({success: false, message: stdout});
+                }
+            }
+        });
+    } else {
+        res.json({success: false});
+    }
+});
+
+module.exports = router;
