2b35ac1c1ed75955240536e4dfaaca9ba1241049
Henry Wong <hwong@dja.com>
2017-09-08 14:35:04 -0400

    Updates!

--- gui/routes/run/index.js
+++ gui/routes/run/index.js
@@ -23,4 +23,28 @@ router.get('/:imageID', function(req, res, next) {
     }
 });
 
+router.get('/:imageID/:networkID'), function(req, res, next) {
+    var regex = /^(\w+|sha256:\w+)$/;
+    if (regex.test(req.params.imageID) && regex.test(req.params.networkID)) {
+        var imageID = req.params.imageID;
+        var networkID = req.params.networkID;
+        if (imageID.indexOf('sha256:') > 0) {
+            imageID = imageID.splice(7);
+        };
+        if (networkID.indexOf('sha256:') > 0) {
+            networkID = networkID.splice(7);
+        };
+        shell.execFile('docker', ['run', '-d', '--network', networkID, imageID], function (error, stdout, stderr) {
+            if (stderr) {
+                res.json({success: false, message: error});
+            } else {
+                res.json({success: true, message: stdout});
+            }
+        });
+    } else {
+        res.json({success: false});
+    };
+});
+
+
 module.exports = router;
