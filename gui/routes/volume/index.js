e8b09303329b17deabb1651b62d82f63fe8a6686
Henry Wong <hwong@dja.com>
2017-09-08 21:58:09 -0400

    remove commented code!

--- gui/routes/volume/index.js
+++ gui/routes/volume/index.js
@@ -3,24 +3,6 @@ var router = express.Router();
 
 var shell = require('child_process');
 
-/* GET home page. */
-/*
-router.get('/ls', function(req, res, next) {
-    // docker volume ls --format "{{.Driver}}\t{{.Name}}\t{{.Scope}}\t{{.Mountpoint}}\t{{.Labels}}"
-    shell.execFile('docker', ['volume', 'ls', '--format', '{{.Driver}}\t{{.Name}}\t{{.Scope}}\t{{.Mountpoint}}\t{{.Labels}}'], function (error, stdout, stderr) {
-        var g = stdout.split("\n");
-        var a = g.length - 1;
-        // DRIVER              VOLUME NAME
-        var d = [];
-        for (var i = 0; i < a; i++) {
-            // parse each one into it own cell
-            d.push(g[i].split("\t"));
-        }
-        res.json({images: d});
-    });
-});
-*/
-
 router.get('/ls', function(req, res, next) {
     shell.execFile('docker', ['volume', 'ls', '--format', '{{json .}}'], function (error, stdout, stderr) {
         var rows = stdout.split("\n");
